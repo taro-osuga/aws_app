0caa09f591b42f1d160c7f6c2f0d7357a3762283
taroosuga <copernix0@gmail.com>
2020-05-25 21:24:29 +0900

    achieveに再修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, 'https://github.com/taro-osuga/aws_app.git'
 # deployするブランチ。デフォルトはmasterなのでなくても可。
 set :branch, ENV['BRANCH'] || 'master'
 # deploy先のディレクトリ。
-set :deploy_to, '/var/www/AWS_app'
+set :deploy_to, '/var/www/achieve'
 # set :deploy_to, '/var/www/AWS_app'
 # シンボリックリンクをはるフォルダ・ファイル
 set :linked_files, %w{.env config/secrets.yml}
